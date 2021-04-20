e75616cf46fa595cfba00a95a41653f1acdf0c47
Rolando Chavarria <>
2021-04-20 16:19:50 +0000

    separación de líneas

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int main() {
  float n1, n2;
  bool salida;
-    cout << "\nMuestra en pantalla True o False si la edad ingresada por dos usuarios es la misma: ";//función del Prográma
+    cout << "\nMuestra en pantalla True o False si la edad ingresada por dos usuarios es la misma: \n";//función del Prográma
     cout << "\nIngrese la edad del usuario #1: ";
     cin >> n1 ;
-        cout << "\nIngrese la edad del usuario #2: ";
-    cin >> n2 ;
+        cout << "Ingrese la edad del usuario #2: ";
+    cin >> n2;
       if (n1==n2){
-       cout << "¿La edad del usuario #1 y #2 es la misma? : " <<"True";
+       cout << "\n¿La edad del usuario #1 y #2 es la misma? : " <<"True";
    return 0;
   }
     cout << "¿La edad del usuario #1 y #2 es la misma? : " <<"False";
